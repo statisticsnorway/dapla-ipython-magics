e30d691955a122d7591fab9e9ff0b550e778a77e
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2020-08-06 21:08:03 +0200

    Move package doc one level down

---
+++ dapla/magics/__init__.py
@@ -0,0 +1,6 @@
+"""IPython magics developed and used by Statistics Norway (SSB). This library contains magics for use in Jupyter
+notebooks. """
+
+__version__ = '0.0.2'
+__author__ = 'Statistics Norway (ssb.no)'
+__all__ = []
